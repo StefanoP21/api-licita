bcb2045010ac7ce9e8eccc7766eb825e43762631
Stefano Palomino <stefanop21@outlook.es>
2025-04-02 20:07:42 -0500

    added opportunity dto

--- src/modules/opportunities/dto/create-opportunity.dto.ts
+++ src/modules/opportunities/dto/create-opportunity.dto.ts
@@ -1,1 +1,30 @@
-export class CreateOpportunityDto {}
+import {
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateOpportunityDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(50)
+  code: string;
+
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  is_followed: boolean;
+
+  @IsNotEmpty()
+  @IsDate()
+  publish_date: Date;
+
+  @IsNotEmpty()
+  @IsDate()
+  close_date: Date;
+}
